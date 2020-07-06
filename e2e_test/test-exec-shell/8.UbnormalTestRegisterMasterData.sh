a23fce61eddb986683dff35c7797e83cd2892329
坂本 <sakamoto@X-2365.local>
2020-07-06 17:51:08 +0900

    Integration argument files

--- e2e_test/test-exec-shell/8.UbnormalTestRegisterMasterData.sh
+++ e2e_test/test-exec-shell/8.UbnormalTestRegisterMasterData.sh
@@ -14,13 +14,13 @@ echo "データセットスキーマを更新 - sysope"
 ./gradlew cucumber_update_data_set_schema_sysope
 
 echo "第三者提供先を登録 - sysope"
-cucumber_register_third_party_sysope
+./gradlew cucumber_register_third_party_sysope
 
 echo "第三者提供先を更新 - sysope"
-cucumber_update_third_party_sysope
+./gradlew cucumber_update_third_party_sysope
 
 echo "Adminが他社の第三者提供先を更新"
-cucumber_update_third_party_admin_notowncompany
+./gradlew cucumber_update_third_party_admin_notowncompany
 
 echo "データリテンションポリシーを登録 - sysope"
 ./gradlew cucumber_register_data_retention_policy_sysope

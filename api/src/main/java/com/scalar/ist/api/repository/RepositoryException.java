6445daec8680951beb1fd251bc672eca9db43d21
ken.yurino <ken.yurino@gmail.com>
2020-07-10 23:18:04 +0900

    delete_unused_directories

--- api/src/main/java/com/scalar/ist/api/repository/RepositoryException.java
+++
@@ -1,7 +0,0 @@
-package com.scalar.ist.api.repository;
-
-public class RepositoryException extends RuntimeException {
-  public RepositoryException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}

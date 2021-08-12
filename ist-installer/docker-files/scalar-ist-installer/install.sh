7339b6e1146fc7fab7f16f7cc32f54d3c0ed543e
Yves Peckstadt <peckstadt.yves@gmail.com>
2021-08-12 17:18:11 +0900

    add log output to script and rename folders

---
+++ ist-installer/docker-files/scalar-ist-installer/install.sh
@@ -0,0 +1,88 @@
+STATUS=0
+
+register_cert(){
+  echo registering certificate...
+  while IFS= read line;
+    do
+      OUTPUT=$($line | sed -n '2 p')
+
+      echo $line
+
+      if  [[ $OUTPUT == *"OK"* ]];
+      then
+          echo "$OUTPUT" certificate has been registered
+      elif [[ $OUTPUT == *"CERTIFICATE_ALREADY_REGISTERED"* ]];
+      then
+        echo "$OUTPUT" the certificate has already been registered and is being skipped
+      else
+        echo "$OUTPUT" certificate registeration failed
+                STATUS=1
+                break
+      fi
+  done < "./register-cert.txt"
+}
+
+
+register_functions(){
+  if [[ $IST_INSTALL_FUNCTIONS == true ]];
+  then
+    echo registering functions...
+    while IFS= read line;
+    do
+      OUTPUT=$($line | sed -n '2 p')
+
+      echo $line
+
+      if  [[ $OUTPUT == *"OK"* ]];
+      then
+        echo "$OUTPUT" the function has been registered
+      elif [[ $OUTPUT == *"CERTIFICATE_ALREADY_REGISTERED"* ]];
+        echo "$OUTPUT" the function has already been registered and is being skipped
+      then
+         echo "$OUTPUT" the function registeration failed
+                STATUS=1
+                break
+      fi
+    done < "./register-functions.txt"
+  fi
+}
+
+register_contracts(){
+  if [[ $IST_INSTALL_CONTRACTS == true ]];
+  then
+    echo registering contracts...
+    while IFS= read line;
+    do
+      OUTPUT=$($line | sed -n '2 p')
+
+
+      echo $line
+
+      if  [[ $OUTPUT == *"OK"* ]];
+      then
+        echo "$OUTPUT" the contract has been registered
+      elif [[ $OUTPUT == *"CERTIFICATE_ALREADY_REGISTERED"* ]];
+        echo "$OUTPUT" the contract has already been registered and is being skipped
+      then
+        echo "$OUTPUT" the contract registeration failed
+        STATUS=1
+        break
+      fi
+    done < "./register-contracts.txt"
+  fi
+}
+
+register_cert
+if [[ $STATUS == 1 ]];
+then
+  exit $STATUS
+fi
+register_functions
+if [[ $STATUS == 1 ]];
+then
+  exit $STATUS
+fi
+register_contracts
+exit $STATUS
+
+

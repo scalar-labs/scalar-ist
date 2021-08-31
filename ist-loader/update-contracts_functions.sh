eaf652f4a025528a215a8fac89c352a4aa59c121
milan <milan.correggio@gmail.com>
2021-08-31 11:17:28 +0900

    delete not needed bash

--- ist-loader/update-contracts_functions.sh
+++
@@ -1,7 +0,0 @@
-rm -rf docker-files/scalar-ist-loader/contracts_and_functions/build
-../contracts_and_functions/gradlew build
-
-SOURCE=../contracts_and_functions/build/classes/java/main/
-TARGET=docker-files/scalar-ist-loader/contracts_and_functions/build/classes/java/main
-
-mkdir -p $TARGET && cp -r $SOURCE $TARGET

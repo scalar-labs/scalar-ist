a3d686d76ee5a3b3ae67554c061048e911e6d5ca
Milan Correggio <milan.correggio@gmail.com>
2021-10-06 18:25:58 +0900

    not run anything if env are not setted

--- ist-loader/docker-files/scalar-ist-loader/install.sh
+++ ist-loader/docker-files/scalar-ist-loader/install.sh
@@ -33,25 +33,29 @@ register_functions(){
 check_env(){
   echo checking if all env variables are setted ...
   if [[ -z "${LEDGER_HOST}" ]]; then
+    $ENV_STATUS=1
     echo LEDGER_HOST variable is not setted
   fi
   if [[ -z "${CLIENT_PROPERTIES_PATH}" ]]; then
+    $ENV_STATUS=1
     echo CLIENT_PROPERTIES_PATH variable is not setted
   fi
 }
-
+ENV_STATUS=0
 check_env
-PROPERTIES="--properties $CLIENT_PROPERTIES_PATH"
-while ! nc -z $LEDGER_HOST 50051
-do
-  echo waiting for ledger
-  sleep 5
-done
-if [[ $IST_INSTALL_CONTRACTS == true ]];then
-    register_cert
-    register_contracts
-fi
-if [[ $IST_INSTALL_FUNCTIONS == true ]];then
-    register_functions
+if [[ $ENV_STATUS==0 ]]; then
+  PROPERTIES="--properties $CLIENT_PROPERTIES_PATH"
+  while ! nc -z $LEDGER_HOST 50051
+  do
+    echo waiting for ledger
+    sleep 5
+  done
+  if [[ $IST_INSTALL_CONTRACTS == true ]];then
+      register_cert
+      register_contracts
+  fi
+  if [[ $IST_INSTALL_FUNCTIONS == true ]];then
+      register_functions
+  fi
 fi
 exit 0

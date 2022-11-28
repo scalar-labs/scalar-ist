ca6efc82ccc2354818ecc69e2e9b7f7361b2a7d7
scalar-boney <58680313+scalar-boney@users.noreply.github.com>
2022-11-28 10:12:17 +0900

    Refactor scalar-ist register function (#50)
    * register functions
    
    * remove unsed function

--- tools/deploy/common.sh
+++ tools/deploy/common.sh
@@ -2,7 +2,7 @@
 
 # constants
 REGISTER_CERTIFICATE_BIN="$ROOTDIR/scalardl-java-client-sdk/bin/register-cert"
-REGISTER_FUNCTION_BIN="$ROOTDIR/scalardl-java-client-sdk/bin/register-function"
+REGISTER_FUNCTIONS_BIN="$ROOTDIR/scalardl-java-client-sdk/bin/register-functions"
 REGISTER_CONTRACTS_BIN="$ROOTDIR/scalardl-java-client-sdk/bin/register-contracts"
 EXECUTE_CONTRACT_BIN="$ROOTDIR/scalardl-java-client-sdk/bin/execute-contract"
 LIST_CONTRACT_BIN="$ROOTDIR/scalardl-java-client-sdk/bin/list-contracts"
@@ -41,32 +41,20 @@ register_certificate() {
   return $status
 }
 
-register_function() {
+register_functions() {
   local properties="$1"
-  if [ "$properties" == '' ]; then
+  if [ "${properties}" == '' ]; then
     return '1'
   fi
 
-  local function_id="$2"
-  if [ "$function_id" == '' ]; then
+  local functions_file="$2"
+  if [ "${functions_file}" == '' ]; then
     return '1'
   fi
 
-  local function_binary_name="$3"
-  if [ "$function_binary_name" == '' ]; then
-    return '1'
-  fi
-
-  local function_file="$4"
-  if [ "$function_file" == '' ]; then
-    return '1'
-  fi
-
-  $REGISTER_FUNCTION_BIN \
-    --config $properties \
-    --function-id $function_id \
-    --function-binary-name $function_binary_name \
-    --function-class-file $function_file
+  $REGISTER_FUNCTIONS_BIN \
+    --config ${properties} \
+    --functions-file ${functions_file}
 
   return $?
 }
